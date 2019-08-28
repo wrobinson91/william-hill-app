f21fc8dc72e7cdd19a7e9cde1cafb382e938cde2
Will Robinson <wrobinson91@gmail.com>
2019-08-28 19:10:44 -0400

    added file of nfl, nba, mlb teams. need to do nhl and mls

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import TeamInfoContainer from '../containers/TeamInfoContainer';
+// not going to use router for now
+// import { BrowserRouter as Router } from 'react-router-dom';
 
 const App = () => (
-  <h2>Hello World!</h2>
+  <>
+    <h2>Hello World!</h2>
+    <TeamInfoContainer />
+  </>
 );
 
 export default App;
---
+++ src/client/containers/TeamInfoContainer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TeamInfoContainer = () => (
+  <section>
+      This is the Team Info Container
+  </section>
+);
+
+export default TeamInfoContainer;
